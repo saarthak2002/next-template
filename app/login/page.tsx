00d0bb6a8f2322c97bd851841140e3f845262fb9
Saarthak Gupta <79454214+saarthak2002@users.noreply.github.com>
2025-03-15 16:09:46 -0400

    added suspense boundary to login form

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,10 +1,13 @@
 import LoginForm from '@/ui/login-form';
+import { Suspense } from 'react';
 
 export default function LoginPage() {
 	return (
 		<div className="flex min-h-svh flex-col items-center justify-center bg-muted p-6 md:p-10">
 			<div className="w-full max-w-sm md:max-w-3xl">
-				<LoginForm />
+				<Suspense>
+					<LoginForm />
+				</Suspense>
 			</div>
 		</div>
 	)
